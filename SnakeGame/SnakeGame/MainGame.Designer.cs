91fb2d47f0986b614bf954f9d15b658614900b6b
NDK <dk.wofts@gmail.com>
2019-12-19 01:15:42 +0700

    run snake game ok

--- SnakeGame/SnakeGame/MainGame.Designer.cs
+++ SnakeGame/SnakeGame/MainGame.Designer.cs
@@ -28,36 +28,258 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.pnlMainBoard = new System.Windows.Forms.Panel();
+            this.bwFPS = new System.ComponentModel.BackgroundWorker();
+            this.pnlMain = new SnakeGame.BasePanelGame();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.tsStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.tsScore = new System.Windows.Forms.ToolStripStatusLabel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.restartGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.chooseModeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.levelToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.classicalToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.modernToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.easyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mediumToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hardToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsHighScore = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel3 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.tsFPS = new System.Windows.Forms.ToolStripStatusLabel();
+            this.pnlMain.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // pnlMainBoard
+            // bwFPS
             // 
-            this.pnlMainBoard.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pnlMainBoard.Location = new System.Drawing.Point(0, 0);
-            this.pnlMainBoard.Name = "pnlMainBoard";
-            this.pnlMainBoard.Size = new System.Drawing.Size(624, 441);
-            this.pnlMainBoard.TabIndex = 0;
-            this.pnlMainBoard.Paint += new System.Windows.Forms.PaintEventHandler(this.pnlMainBoard_Paint);
+            this.bwFPS.WorkerReportsProgress = true;
+            this.bwFPS.WorkerSupportsCancellation = true;
+            this.bwFPS.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bwFPS_DoWork);
+            this.bwFPS.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.bwFPS_ProgressChanged);
+            this.bwFPS.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.bwFPS_RunWorkerCompleted);
+            // 
+            // pnlMain
+            // 
+            this.pnlMain.Controls.Add(this.statusStrip1);
+            this.pnlMain.Controls.Add(this.menuStrip1);
+            this.pnlMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlMain.Location = new System.Drawing.Point(0, 0);
+            this.pnlMain.Name = "pnlMain";
+            this.pnlMain.Size = new System.Drawing.Size(624, 441);
+            this.pnlMain.TabIndex = 0;
+            this.pnlMain.Paint += new System.Windows.Forms.PaintEventHandler(this.pnlMain_Paint);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel3,
+            this.tsFPS,
+            this.toolStripStatusLabel2,
+            this.tsHighScore,
+            this.tsStatus,
+            this.toolStripStatusLabel1,
+            this.tsScore});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 417);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(624, 24);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // tsStatus
+            // 
+            this.tsStatus.BorderSides = ((System.Windows.Forms.ToolStripStatusLabelBorderSides)((System.Windows.Forms.ToolStripStatusLabelBorderSides.Left | System.Windows.Forms.ToolStripStatusLabelBorderSides.Right)));
+            this.tsStatus.Name = "tsStatus";
+            this.tsStatus.Size = new System.Drawing.Size(416, 19);
+            this.tsStatus.Spring = true;
+            this.tsStatus.Text = "None";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(42, 19);
+            this.toolStripStatusLabel1.Text = "Score :";
+            // 
+            // tsScore
+            // 
+            this.tsScore.BorderSides = System.Windows.Forms.ToolStripStatusLabelBorderSides.Right;
+            this.tsScore.Name = "tsScore";
+            this.tsScore.Size = new System.Drawing.Size(17, 19);
+            this.tsScore.Text = "0";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(624, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newGameToolStripMenuItem,
+            this.restartGameToolStripMenuItem,
+            this.chooseModeToolStripMenuItem,
+            this.levelToolStripMenuItem,
+            this.quitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // newGameToolStripMenuItem
+            // 
+            this.newGameToolStripMenuItem.Name = "newGameToolStripMenuItem";
+            this.newGameToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.newGameToolStripMenuItem.Text = "New Game";
+            this.newGameToolStripMenuItem.Click += new System.EventHandler(this.newGameToolStripMenuItem_Click);
+            // 
+            // restartGameToolStripMenuItem
+            // 
+            this.restartGameToolStripMenuItem.Name = "restartGameToolStripMenuItem";
+            this.restartGameToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.restartGameToolStripMenuItem.Text = "Restart Game";
+            this.restartGameToolStripMenuItem.Click += new System.EventHandler(this.restartGameToolStripMenuItem_Click);
+            // 
+            // chooseModeToolStripMenuItem
+            // 
+            this.chooseModeToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.classicalToolStripMenuItem,
+            this.modernToolStripMenuItem});
+            this.chooseModeToolStripMenuItem.Name = "chooseModeToolStripMenuItem";
+            this.chooseModeToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.chooseModeToolStripMenuItem.Text = "Choose Mode";
+            // 
+            // levelToolStripMenuItem
+            // 
+            this.levelToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.easyToolStripMenuItem,
+            this.mediumToolStripMenuItem,
+            this.hardToolStripMenuItem});
+            this.levelToolStripMenuItem.Name = "levelToolStripMenuItem";
+            this.levelToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.levelToolStripMenuItem.Text = "Level";
+            // 
+            // quitToolStripMenuItem
+            // 
+            this.quitToolStripMenuItem.Name = "quitToolStripMenuItem";
+            this.quitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.quitToolStripMenuItem.Text = "Quit";
+            this.quitToolStripMenuItem.Click += new System.EventHandler(this.quitToolStripMenuItem_Click);
+            // 
+            // classicalToolStripMenuItem
+            // 
+            this.classicalToolStripMenuItem.Name = "classicalToolStripMenuItem";
+            this.classicalToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.classicalToolStripMenuItem.Text = "Classical";
+            this.classicalToolStripMenuItem.Click += new System.EventHandler(this.classicalToolStripMenuItem_Click);
+            // 
+            // modernToolStripMenuItem
+            // 
+            this.modernToolStripMenuItem.Name = "modernToolStripMenuItem";
+            this.modernToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.modernToolStripMenuItem.Text = "Modern";
+            this.modernToolStripMenuItem.Click += new System.EventHandler(this.modernToolStripMenuItem_Click);
+            // 
+            // easyToolStripMenuItem
+            // 
+            this.easyToolStripMenuItem.Name = "easyToolStripMenuItem";
+            this.easyToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.easyToolStripMenuItem.Text = "Easy";
+            this.easyToolStripMenuItem.Click += new System.EventHandler(this.easyToolStripMenuItem_Click);
+            // 
+            // mediumToolStripMenuItem
+            // 
+            this.mediumToolStripMenuItem.Name = "mediumToolStripMenuItem";
+            this.mediumToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.mediumToolStripMenuItem.Text = "Medium";
+            this.mediumToolStripMenuItem.Click += new System.EventHandler(this.mediumToolStripMenuItem_Click);
+            // 
+            // hardToolStripMenuItem
+            // 
+            this.hardToolStripMenuItem.Name = "hardToolStripMenuItem";
+            this.hardToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.hardToolStripMenuItem.Text = "Hard";
+            this.hardToolStripMenuItem.Click += new System.EventHandler(this.hardToolStripMenuItem_Click);
+            // 
+            // tsHighScore
+            // 
+            this.tsHighScore.Name = "tsHighScore";
+            this.tsHighScore.Size = new System.Drawing.Size(13, 19);
+            this.tsHighScore.Text = "0";
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.BorderSides = System.Windows.Forms.ToolStripStatusLabelBorderSides.Left;
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(72, 19);
+            this.toolStripStatusLabel2.Text = "HighScore :";
+            // 
+            // toolStripStatusLabel3
+            // 
+            this.toolStripStatusLabel3.BorderSides = System.Windows.Forms.ToolStripStatusLabelBorderSides.Left;
+            this.toolStripStatusLabel3.Name = "toolStripStatusLabel3";
+            this.toolStripStatusLabel3.Size = new System.Drawing.Size(36, 19);
+            this.toolStripStatusLabel3.Text = "FPS :";
+            // 
+            // tsFPS
+            // 
+            this.tsFPS.Name = "tsFPS";
+            this.tsFPS.Size = new System.Drawing.Size(13, 19);
+            this.tsFPS.Text = "0";
             // 
             // MainGame
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(624, 441);
-            this.Controls.Add(this.pnlMainBoard);
+            this.Controls.Add(this.pnlMain);
+            this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "MainGame";
             this.Text = "SnakeGame";
             this.Load += new System.EventHandler(this.MainGame_Load);
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.MainGame_KeyDown);
+            this.pnlMain.ResumeLayout(false);
+            this.pnlMain.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Panel pnlMainBoard;
+        private System.ComponentModel.BackgroundWorker bwFPS;
+        private BasePanelGame pnlMain;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel tsStatus;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel tsScore;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel3;
+        private System.Windows.Forms.ToolStripStatusLabel tsFPS;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.ToolStripStatusLabel tsHighScore;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newGameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem restartGameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem chooseModeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem classicalToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem modernToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem levelToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem easyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mediumToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hardToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem quitToolStripMenuItem;
     }
 }
 
--- SnakeGame/SnakeGame/MainGame.cs
+++ SnakeGame/SnakeGame/MainGame.cs
@@ -6,6 +6,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using SnakeGame.Model;
@@ -14,81 +15,220 @@ namespace SnakeGame
 {
     public partial class MainGame : Form
     {
-        const int DIRECTION = 10;
-        Queue<Point> snake = new Queue<Point>();
-        Random random = new Random();
-        Point direction;
-        Point food;
-        int cols, rows, w, h;
+        const int UNIT = 10;
+        Brush brsnake = new SolidBrush(Color.Red);
+        Brush brfood = new SolidBrush(Color.Blue);
+        List<Point> snake = new List<Point>();
+        Random rand = new Random();
+        Point food = new Point();
+        Point dir = new Point();
+        Point s0 = new Point();
+        Point s = new Point();
+        bool gameover = false;
+        int w, h, cols, rows, fps;
+        int score, highscore;
 
         public MainGame()
         {
             InitializeComponent();
-            cols = 50;
-            rows = 50;
-            w = this.Width / cols;
-            h = this.Height / rows;
-            food = new Point(random.Next(50), random.Next(50));
+            dir = new Point(0, 0);
+            w = pnlMain.Width;
+            h = pnlMain.Height;
+            cols = w / UNIT;
+            rows = h / UNIT;
+            score = 0;
+            fps = 30;
         }
 
         private void MainGame_Load(object sender, EventArgs e)
         {
-            CreateSnake();
+            AddSnake(new Point((cols / 2 - 2) * UNIT, h / 2));
+            AddSnake(new Point((cols / 2 - 1) * UNIT, h / 2));
+            AddSnake(new Point((cols / 2) * UNIT, h / 2));
             CreateFood();
-            pnlMainBoard.Refresh();
-        }
-
-        private void CreateSnake()
-        {
-            Point p1 = new Point(random.Next(50), random.Next(50));
-            Point p2 = new Point(p1.X - 1, p1.Y);
-            Point p3 = new Point(p2.X - 1, p2.Y);
-            snake.Enqueue(p1);
-            snake.Enqueue(p2);
-            snake.Enqueue(p3);
         }
 
         private void MainGame_KeyDown(object sender, KeyEventArgs e)
         {
-            switch(e.KeyCode)
+            switch (e.KeyCode)
             {
+                case Keys.Up:
+                    dir.X = 0;
+                    if (dir.Y != UNIT)
+                        dir.Y = -UNIT;
+                    break;
+                case Keys.Down:
+                    dir.X = 0;
+                    if (dir.Y != -UNIT)
+                        dir.Y = UNIT;
+                    break;
+                case Keys.Left:
+                    if (dir.X != UNIT)
+                        dir.X = -UNIT;
+                    dir.Y = 0;
+                    break;
+                case Keys.Right:
+                    if (dir.X != -UNIT)
+                        dir.X = UNIT;
+                    dir.Y = 0;
+                    break;
+                case Keys.P:
+                    if (bwFPS.IsBusy)
+                    {
+                        bwFPS.CancelAsync();
+                    }
+                    break;
             }
+            if (!bwFPS.IsBusy)
+            {
+                bwFPS.RunWorkerAsync();
+            }
+        }
+
+        private void pnlMain_Paint(object sender, PaintEventArgs e)
+        {
+            foreach (Point p in snake)
+            {
+                e.Graphics.FillEllipse(brsnake, p.X, p.Y, 10, 10);
+            }
+            e.Graphics.FillEllipse(brfood, food.X, food.Y, 10, 10);
+        }
+
+        private void bwFPS_DoWork(object sender, DoWorkEventArgs e)
+        {
+            while (!gameover)
+            {
+                if (bwFPS.CancellationPending)
+                {
+                    e.Cancel = true;
+                    return;
+                }
+                bwFPS.ReportProgress(0);
+                Thread.Sleep(fps * 10);
+            }
+        }
+
+        private void bwFPS_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            tsStatus.Text = "PLAYING";
+            tsScore.Text = score.ToString();
+            tsFPS.Text = fps.ToString();
+            SnakeMove();
+        }
+
+        private void bwFPS_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Cancelled)
+            {
+                tsStatus.Text = "PAUSED";
+            }
+            else
+                tsStatus.Text = "GAME OVER";
+        }
+
+        private void SnakeMove()
+        {
+            s = new Point(snake[0].X + dir.X, snake[0].Y + dir.Y);
+            foreach (Point p in snake)
+            {
+                if (s == p)
+                    gameover = true;
+            }
+            if (s == food)
+            {
+                AddSnake(s);
+                score += 10;
+                CreateFood();
+            }
+            else
+            {
+                if (s.X == this.Width)
+                    s.X = 0;
+                if (s.Y == this.Height)
+                    s.Y = 0;
+                for (int i = 0; i < snake.Count; i++)
+                {
+                    s0 = snake[i];
+                    snake[i] = s;
+                    s = s0;
+                }
+            }
+            pnlMain.Refresh();
+        }
+
+        private void AddSnake(Point hs)
+        {
+            if (snake.Count == 0)
+                snake.Add(hs);
+            else
+            {
+                s = hs;
+                for (int i = 0; i < snake.Count; i++)
+                {
+                    s0 = snake[i];
+                    snake[i] = s;
+                    s = s0;
+                }
+                snake.Add(s);
+            }
+            pnlMain.Refresh();
         }
 
         private void CreateFood()
         {
-            if (CheckFood())
-                food = new Point(random.Next(50), random.Next(50));
-        }
-
-        private bool CheckFood()
-        {
+            CheckFood:
+            int x = rand.Next(cols);
+            int y = rand.Next(rows);
+            Point k = new Point(x * UNIT, y * UNIT);
             foreach (Point p in snake)
             {
-                if (food == p)
-                    return false;
+                if (p == k)
+                {
+                    goto CheckFood;
+                }
             }
-            return true;
+            food = k;
+            pnlMain.Refresh();
         }
 
-        private void pnlMainBoard_Paint(object sender, PaintEventArgs e)
+        private void newGameToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            //for (int x = 0; x < this.Width; x += w)
-            //{
-            //    e.Graphics.DrawLine(new Pen(Color.Red), x, 0, x, this.Height);
-            //}
-            //for (int y = 0; y < this.Height; y += h)
-            //{
-            //    e.Graphics.DrawLine(new Pen(Color.Red), 0, y, this.Width, y);
-            //}
-            if (food != null)
-            {
-                foreach(Point p in snake)
-                {
-                    e.Graphics.DrawEllipse(new Pen(Color.Blue), p.X * w, p.Y * h, w, h);
-                }
-                e.Graphics.DrawEllipse(new Pen(Color.Blue), food.X * w, food.Y * h, w, h);
-            }
+
+        }
+
+        private void restartGameToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void classicalToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void modernToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void easyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void mediumToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void hardToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void quitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
